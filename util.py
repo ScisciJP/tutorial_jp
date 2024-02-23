64f296907ae5b98ec430e501fce0d6336e24dec4
psmuler <94701070+psmuler@users.noreply.github.com>
2024-02-23 15:35:09 +0900

    add getAll and moved all samplecode to separate file

--- util.py
+++ util.py
@@ -1,4 +1,6 @@
 from pyalex import Works
+import tqdm
+
 
 def citation_snowball(id):
     return id
@@ -8,3 +10,11 @@ def citation_snowball(id):
     #     if len(page)>0:
     #         reference += list(map(lambda x: x["id"], page))
     # reference
+
+def getAll(pyalexObj):
+    # pyalexObj.count()
+    pager = pyalexObj.paginate(per_page=200)
+    arr = []
+    for page in tqdm.tqdm(pager):
+        arr += page
+    return arr
