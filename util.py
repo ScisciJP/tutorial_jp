c6ef6381bd60ce6827fa0dd4f50b7744ff31155e
psmuler <94701070+psmuler@users.noreply.github.com>
2024-03-08 13:45:11 +0900

    citation clustering

--- util.py
+++ util.py
@@ -1,23 +1,13 @@
-from pyalex import Works, Authors
 import tqdm
-import pandas as pd
 
-def citation_snowball(id):
-    return id
-    # pager = Works().filter(cited_by=id).paginate(per_page=20)
-    # reference = []
-    # for page in pager:
-    #     if len(page)>0:
-    #         reference += list(map(lambda x: x["id"], page))
-    # reference
-
-def getAll(pyalexObj):
-    # pyalexObj.count()
-    pager = pyalexObj.paginate(method="page",per_page=200)
+def getAll(pyalexObj, verbous=True):
+    count = pyalexObj.count()
+    print(f"Fetching {count} objects. it may takes {round(count/200/60*3,2)} minutes")
+    if count>10000:
+        pager = pyalexObj.paginate(per_page=200, n_max=None)
+    else:
+        pager = pyalexObj.paginate(method="page",per_page=200)
     arr = []
-    for page in tqdm.tqdm(pager):
+    for page in tqdm.tqdm(pager,disable= not verbous):
         arr += page
     return arr
-
-# obj = Works().filter(institution={"id":"https://openalex.org/I74801974"}, publication_year=">2020",cited_by_count=">10",primary_topic={"field":{"id":11}}).sample(500,seed=42).select(["cited_by_count"])
-# getAll(obj)
